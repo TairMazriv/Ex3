3e437154674c33b69cc356aab992855cdcc05999
TairManzaly <73856271+TairManzaly@users.noreply.github.com>
2022-01-04 20:44:49 +0200

    Add files via upload

--- Ex3/src/GraphAlgo.py
+++ Ex3/src/GraphAlgo.py
@@ -1,16 +1,18 @@
 import json
 import sys
 from collections import defaultdict
+from random import random
 from typing import List
 import threading, queue
-import DiGraph, GraphInterface, GraphAlgoInterface, Node, Edge
+
+from matplotlib import pyplot as plt
+
+from src import DiGraph, GraphInterface, GraphAlgoInterface, Node, Edge
 from DiGraph import DiGraph
 import GraphInterface, Node
 from GraphAlgoInterface import GraphAlgoInterface
 from DiGraph import DiGraph
 from Node import Node
-import random
-import matplotlib.pyplot as plt
 
 
 class GraphAlgo(GraphAlgoInterface):
@@ -18,10 +20,10 @@ class GraphAlgo(GraphAlgoInterface):
         self._GRAPH = g
 
     def __str__(self):
-        return f"graph:{self._GRAPH}"
+        return f"graph:{self.graph}"
 
     def __repr__(self):
-        return f"graph:{self._GRAPH}"
+        return f"graph:{self.graph}"
 
     def get_graph(self) -> GraphInterface:
         return self._GRAPH
@@ -32,8 +34,11 @@ class GraphAlgo(GraphAlgoInterface):
                 load = json.load(fn)
                 # graph = DiGraph()
                 for node in load["Nodes"]:
-                    pos = (node["pos"]).split(',')
-                    self._GRAPH.add_node(node["id"], pos)
+                    if (len(node) == 1):
+                        self._GRAPH.add_node(node["id"])
+                    else:
+                        pos = (node["pos"]).split(',')
+                        self._GRAPH.add_node(node["id"], pos)
                 for edge in load["Edges"]:
                     self._GRAPH.add_edge(edge["src"], edge["dest"], edge["w"])
                 # GraphAlgo(self, graph)
@@ -55,110 +60,19 @@ class GraphAlgo(GraphAlgoInterface):
             print(data)
             with open(file_name, 'w') as outfile:
                 json.dump(data, outfile)
+            return True
         except IOError as er:
             print(er)
             return False
 
-        def shortest_path(self, id1: int, id2: int) -> (float, list):
-        a = self.dijkstra(id1, id2)
-        visited = a[0]
-        path = a[1]
-        # if there is no path
-        if visited.get(id2) == float('inf'):
-            return float('inf'), []
-        ans = []
-
-        node = id2
-
-        while node != id1:
-            ans.append(node)
-            node = visited.get(node)
-
-        ans.append(id1)
-        result = ans[::-1]
-
-        return a[0][id2], result
-
-    def TSP(self, node_lst: List[int]) -> (List[int], float):
-        citiesdata = []
-        result = []
-        same, place = 0, 0
-        for i in range(len(node_lst)):
-            citiesdata.append(node_lst[i])
-
-        list = []
-        temp = node_lst[0]
-        result.append(self._GRAPH.nodes.get(citiesdata.pop(0)))
-        while len(citiesdata) >= 1:
-            min = sys.Maxint
-            same = -1
-            place = -1
-            for i in range(len(citiesdata)):
-                open = citiesdata[i]
-                if self.shortest_path(temp, open)[0] < min:
-                    min = self.shortest_path(temp, open)[0]
-                    same = open
-                    place = i
-            list = self.shortest_path(temp, same)[1]
-            while len(list) >= 1:
-                if list[0] not in result:
-                    result.append(list[0])
-                list.remove(0)
-            q = citiesdata[place]
-            temp = self._GRAPH.nodes.get(q)
-            citiesdata.remove(citiesdata[place])
-            if len(citiesdata) == 1 and self._GRAPH.nodes.get(same + 1) not in result:
-                result.append(self._GRAPH.nodes.get(same + 1))
-        return result
-
-    def centerPoint(self) -> (int, float):
-        min = sys.float_info.max
-        max = sys.float_info.min
-        ans = 0
-        for i in range(len(self._GRAPH.nodes)):
-            for j in range(len(self._GRAPH.nodes)):
-                if j != i:
-                    temp = self.shortest_path(j, i)[0]
-                    if temp > max:
-                        max = temp
-            center = max
-            if center < min:
-                min = center
-                ans = self._GRAPH.nodes[i]
-        return (ans, min)
-
-    def plot_graph(self) -> None:
-        for n in self._GRAPH.nodes.values():
-            if n.getPos() != None:
-                x = n.getPos()[0]
-                y = n.getPos()[1]
-                z = n.getPos()[2]
-            else:
-                x = random.randrange(0, 100)
-                y = random.randrange(0, 100)
-                z = random.randrange(0, 100)
-                n.setPos((x, y, z))
-            plt.plot(float(x), float(y), markersize=4, marker="o", color="black")
-            plt.text(float(x), float(y), n.getId(), color="blue", fontsize=8)
-        for e in self._GRAPH.edges.values():
-            src = self._GRAPH.nodes.get(e["src"])
-            dest = self._GRAPH.nodes.get(e["dest"])
-            srcX = src.getPos()[0]
-            srcY = src.getPos()[1]
-            destX = dest.getPos()[0]
-            destY = dest.getPos()[1]
-            plt.annotate("", xy=(float(srcX), float(srcY)), xytext=(float(destX), float(destY)),
-                         arrowprops=dict(arrowstyle="-", edgecolor="black", lw=1.0))
-        plt.show()
-
-       def dijkstra(self, start: int, dest: int) -> (dict, list):
+    def dijkstra(self, start: int) -> (dict, list):
         unvisited = list(self._GRAPH.nodes.keys())
         visited = {i: float('inf') for i in unvisited}
         path = {}
         visited[start] = 0
         while unvisited:
             minNode = None
-        # let's find the node with the lowest weight value
+            # let's find the node with the lowest weight value
             for node in unvisited:
                 if minNode == None:
                     minNode = node
@@ -174,30 +88,118 @@ class GraphAlgo(GraphAlgoInterface):
                 oni = m["other_node_id"]
                 if value < visited[oni]:
                     visited[oni] = value
-                    visited[oni] = minNode
+                    path[oni] = minNode
 
             unvisited.remove(minNode)
 
-        return visited, path
+        return path, visited
 
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        a = self.dijkstra(id1)
+        visited = a[1]
+        path = a[0]
+        # if there is no path
+        if visited.get(id2) == float('inf'):
+            return float('inf'), []
+        ans = []
 
-if __name__ == '__main__':
-    g_algo = GraphAlgo()
-    file = r"C:\Users\User\PycharmProjects\pythonProject\Ex3\data\A5.json"
-    g_algo.load_from_json(file)
-    print(g_algo.get_graph())
-    # edges = [{"src": 0, "w": 1, "dest": 1}, {"src": 1, "w": 2, "dest": 4}]
-    # , {"src": 1, "dest": 2}: Edge(1, 1, 2), {"src": 2, "dest": 0}: Edge(2, 1, 0)
-    # nodes = {0: Node("0,1,2", 0), 1: Node("2,3,5", 1), 2: Node("0.3,2,1", 2)}
+        node = id2
+
+        while node != id1:
+            ans.append(node)
+            node = path.get(node)
+
+        ans.append(id1)
+        result = ans[::-1]
+
+        return a[1][id2], result
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        citiesdata = [j for j in node_lst]
+        ans = 0
+        result = []
+        t = node_lst[0].getPos()
+        result.append(citiesdata[0])
+        citiesdata.remove(citiesdata[0])
+        while len(citiesdata) >= 1:
+            min = sys.maxsize
+            same = -1
+            place = -1
+            for i in range(len(citiesdata)):
+                open = citiesdata[i]
+                short = (self.shortest_path(t, open))[0]
+                if short < min:
+                    min = short
+                    same = open
+                    place = i
+            list = self.shortest_path(t, same)[1]
+            while len(list) >= 1:
+                if list[0] not in result:
+                    result.append(list[0])
+                list.remove(list[0])
+            q = citiesdata[place]
+            t = q
+            citiesdata.remove(citiesdata[place])
+            if len(citiesdata) == 1 and self._GRAPH.nodes.get(same + 1) not in result:
+                result.append(same + 1)
+            for i in self._GRAPH.edges.values():
+                ans = ans + i["w"]
+        return result, ans
+
+    def centerPoint(self) -> (int, float):
+        max = 0
+        min = float('inf')
+        list = []
+
+        for i in range(len(self._GRAPH.nodes)):
+            max = 0
+            dist = self.dijkstra(i)[1]
+            for j in range(len(dist)):
+
+                if dist[j] > max:
+                    max = dist[j]
+            list.insert(i, max)
+
+        for i in range(len(list)):
+            if min > list[i]:
+                min = list[i]
+                node = i
+
+        return (node, min)
+        # return (ans, min)
+
+    def plot_graph(self) -> None:
+        for n in self._GRAPH.nodes.values():
+            if n.getPos() != None:
+                x = n.getPos()[0]
+                y = n.getPos()[1]
+                z = n.getPos()[2]
+            else:
+                x = random.randrange(0, 100)
+                y = random.randrange(0, 100)
+                z = random.randrange(0, 100)
+                n.setPos((x, y, z))
+            plt.plot(float(x), float(y), markersize=4, marker="o", color="red")
+            # plt.text(float(x), float(y), str(n.getId), color="blue", fontsize=6)
+        for e in self._GRAPH.edges.values():
+            src = self._GRAPH.nodes.get(e["src"])
+            dest = self._GRAPH.nodes.get(e["dest"])
+            srcX = src.getPos()[0]
+            srcY = src.getPos()[1]
+            destX = dest.getPos()[0]
+            destY = dest.getPos()[1]
+            plt.annotate("", xy=(float(srcX), float(srcY)), xytext=(float(destX), float(destY)),
+                         arrowprops=dict(arrowstyle="<-", edgecolor="yellow", lw=1.0))
+        plt.show()
+
+# if __name__ == '__main__':
+#     edges = [{"src": 0, "w": 1, "dest": 1}, {"src": 1, "w": 2, "dest": 4}]
+#     # , {"src": 1, "dest": 2}: Edge(1, 1, 2), {"src": 2, "dest": 0}: Edge(2, 1, 0)
+#     nodes = {0: Node("0,1,2", 0), 1: Node("2,3,5", 1), 2: Node("0.3,2,1", 2)}
 # d = DiGraph(nodes, edges)
 # print(d)
 # g_algo = GraphAlgo(d)
-# g_algo.addNode(0)
-# g_algo.addNode(1)
-# g_algo.addNode(2)
-# g_algo.addEdge(0, 1, 1)
-# g_algo.addEdge(1, 2, 4)
-# print(g_algo.dijkstra(d,1, 2))
+# print(g_algo.shortest_path(0,2))
 # g_algo.shortest_path()
 # #        (1, [0, 1])
 # #        >>> g_algo.shortestPath(0,2)
--- Ex3/src/test_GraphAlgo.py
+++ Ex3/src/test_GraphAlgo.py
@@ -5,7 +5,7 @@ import json
 
 
 g_algo = GraphAlgo()
-file = r"C:\Users\tairm\PycharmProjects\Ex3\src\A0.json"
+file = r"C:\Users\tairm\PycharmProjects\Ex3\src\A1.json"
 class TestGraphAlgo(TestCase):
     def test_get_graph(self):
         self.assertEqual(g_algo._GRAPH, g_algo.get_graph())
@@ -14,16 +14,31 @@ class TestGraphAlgo(TestCase):
         self.assertTrue(g_algo.load_from_json(file))
 
     def test_save_to_json(self):
-        self.fail()
+        # self.fail()
+        self.assertTrue(g_algo.save_to_json('saved_'+r"C:\Users\tairm\PycharmProjects\Ex3\src\A0.json"))
+
 
     def test_shortest_path(self):
-        self.fail()
+        g1_algo = GraphAlgo()
+        file = r"C:\Users\tairm\PycharmProjects\Ex3\src\A0.json"
+        g1_algo.load_from_json(file)
+        dist, path = g1_algo.shortest_path(0,1)
+        self.assertEqual(g1_algo.shortest_path(0,1), (dist, path))
+        # self.fail()
 
     def test_tsp(self):
-        self.fail()
+        g_algo = GraphAlgo()
+        file = r"C:\Users\tairm\PycharmProjects\Ex3\src\A3.json"
+        g_algo.load_from_json(file)
+        result, answer = (
+        [0, 21, 22, 23, 24, 25, 26, 8, 7, 44, 43, 42, 41, 40, 39, 17, 14, 15, 38, 37, 36, 35, 34, 33, 32, 2, 3, 31, 30,
+         13, 12, 11, 20, 19, 18, 10, 9, 1, 16, 6, 5, 28, 4, 29, 48, 47, 46, 45, 46, 27], 8459.737919535499)
 
-    def test_center_point(self):
-        self.fail()
+        self.assertEqual(g_algo.TSP(g_algo.get_graph().nodes), (result, answer))
 
-    def test_plot_graph(self):
-        self.fail()
+    def test_centerPoint(self):
+        g_algo = GraphAlgo()
+        file = r"C:\Users\tairm\PycharmProjects\Ex3\src\A0.json"
+        g_algo.load_from_json(file)
+        node, min= 7, 6.806805834715163
+        self.assertEqual(g_algo.centerPoint(), (node, min))
